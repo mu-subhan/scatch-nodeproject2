dcc4c4ec2c344d133443915f9704098385130e52
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2024-10-18 19:01:29 +0500

    created owner accoutn and handle releated stuff

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose')
+const dbgr = require("debug")("development:mongoose")
 const uri = process.env.MONGODB_URI;
-console.log("MongoDB URI:", uri); // Add this line
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-        console.log("Connected to MongoDB Atlas");
+        dbgr("Connected to MongoDB Atlas");
     })
     .catch(err => {
-        console.error("MongoDB connection error:", err);
+        dbgr("MongoDB connection error:", err);
     });
+// first install debug if you want to omit nothing can be happend in console you can use 
+// export DEBUG=
+
+// if you want to see all developement releated then run below command in terminal 
+// export DEBUG=development:*
