a98bfd782298d6f5402356b8545bc491c667ed4f
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2024-10-19 00:08:26 +0500

    resolved previous issue releated to routes

--- app.js
+++ app.js
@@ -4,21 +4,35 @@ const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
 const path = require("path");
-const db = require('./config/mongoose-connection')
+const db = require('./config/mongoose-connection');
 const ownersRouter = require("./routes/ownersRouter");
 const productsRouter = require("./routes/productsRouter");
 const usersRouter = require("./routes/usersRouter");
 
-
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname,"public")));
-app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
 
-app.use("/owners",ownersRouter);
-app.use("/users",usersRouter);
-app.use("/products",productsRouter);
+app.get("/", (req, res) => {
+    res.send("Welcome to the Home Page!");
+});
+
+// Routes
+app.use("/owners", ownersRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Something went wrong!");
+});
 
 
-app.listen(3000);
+// Start the server
+const PORT = 3000; // Use environment variable or default to 3000
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
