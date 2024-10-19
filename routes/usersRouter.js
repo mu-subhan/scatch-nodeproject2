f7ea92e5088119d5ab38cefff3759c7856dd7d0f
Muhammad Subhan <muhammadsubhan8934@gmail.com>
2024-10-19 23:07:56 +0500

    perfom hashing using bcrypt

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,23 +1,34 @@
 const express = require("express");
 const router = express.Router();
 const userModel = require("../models/user-model")
+const bcrypt = require('bcryptjs')
 
 router.get('/',function(req,res){
     res.send("it is working");
 });
 
 
+
+
 //  route is users/register 
-router.post("/register",async function(req,res){
+router.post("/register", function(req,res){
     try{
         let {fullname,password,email}= req.body;
 
-   let user = await userModel.create({
-        fullname,
-        password,
-        email,
-    });
-    res.send(user);
+    bcrypt.genSalt(10,function(err,salt){
+        bcrypt.hash(password,salt,async function (err,hash){
+            if(err) return res.send(err.message);
+            else{
+               let user = await userModel.create({
+                    fullname,
+                    password,
+                    email,
+                });
+                res.send(user);
+            }
+            })
+    })
+
     } catch(err){
         res.send(err.message);
         
